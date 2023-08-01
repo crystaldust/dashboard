c7f7a48ef124212c1535a0c1217abd9be3c18d63
Zhen Ju <juzhenatpku@gmail.com>
2023-08-01 15:10:13 +0800

    feat(company behavior): Add key for each row in the tables. Construct the data from contributor list SQL result by reading the col names, and update the related columen references

--- src/pages/CompanyBehavior/sql/index.tsx
+++ src/pages/CompanyBehavior/sql/index.tsx
@@ -134,34 +134,49 @@ export function contributorsSql(owner, repo, company, dateRange = undefined, dir
 with '${owner}' as owner, '${repo}' as repo, '${company}' as company
 select search_key__owner,
        search_key__repo,
+       groupArray(author_name) as names,
        contributor,
-       max(authored_date)     as last_active_time,
-       sum(commit_insertions) as total_insertions,
-       sum(commit_deletions)  as total_deletions,
-       sum(commit_lines)      as total_commit_lines,
-       count()                as commit_count
+       max(last_active_time)   as last_active_time,
+       sum(total_insertions)   as total_insertions,
+       sum(total_deletions)    as total_deletions,
+       sum(total_commit_lines) as total_commit_lines,
+       sum(commit_count)       as commit_count
 from (select search_key__owner,
              search_key__repo,
-             toString(author__logins) as contributor,
-             hexsha,
-             authored_date,
-             commit_insertions,
-             commit_deletions,
-             commit_lines
-      from dir_label_new_test
-      where search_key__owner = owner
-        and search_key__repo = repo
-        and author_company = company
-        ${dateRangeClause}
-        ${dirClause}
-        and author__id != 0
-      group by search_key__owner, search_key__repo, author__logins, hexsha, authored_date, commit_insertions,
-               commit_deletions, commit_lines)
+             author_name,
+             contributor,
+             max(authored_date)     as last_active_time,
+             sum(commit_insertions) as total_insertions,
+             sum(commit_deletions)  as total_deletions,
+             sum(commit_lines)      as total_commit_lines,
+             count()                as commit_count
+      from (
+               select search_key__owner,
+                      search_key__repo,
+                      author_name,
+                      author__logins as contributor,
+                      hexsha,
+                      authored_date,
+                      commit_insertions,
+                      commit_deletions,
+                      commit_lines
+               from dir_label_new_test
+               where search_key__owner = owner
+                 and search_key__repo = repo
+                 and author_company = company
+                 ${dateRangeClause}
+                 ${dirClause}
+                 and author__id != 0
+               group by search_key__owner, search_key__repo, author_name, author__logins, hexsha, authored_date,
+                        commit_insertions,
+                        commit_deletions, commit_lines)
+      group by search_key__owner, search_key__repo, contributor, author_name)
 group by search_key__owner, search_key__repo, contributor
 union all
 select search_key__owner,
        search_key__repo,
-       author_name,
+       [author_name]          as names,
+       []                     as contributor,
        max(authored_date)     as last_active_time,
        sum(commit_insertions) as total_insertions,
        sum(commit_deletions)  as total_deletions,
